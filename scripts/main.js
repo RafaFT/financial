dc93d57a6bdf3c9659fc2b4a602c71a900bcbc5c
Rafael F Trad <rafael.ftrad@gmail.com>
2019-07-21 15:15:21 -0300

    fix: Dynamic behavior of hidding and showing the input column 'Porcentagem CDI' now works on reset button is pressed, and not just when the select value changes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,31 @@
-/* Number of column span of the select indices element */
-let selectIndices = document.querySelector('select');
+/* Adjust GUI based on the value of the indices selected */
+let formCalculator = document.querySelector('form');
+let selectElement = document.querySelector('select');
 let selectIndicesColumn = document.querySelector('p#select-indices');
 let selectCdiColumn = document.querySelector('p#cdi-percentage');
 
-selectIndices.onchange = function() {
-  let indicesValue = selectIndices.value;
-  if (indicesValue != 12) {
+function toggleCdiColumn(on) {
+  /* If true, hide column 'Porcentagem CDI'*/
+  if (on == true) {
     selectIndicesColumn.style.gridColumn = '1 / -1';
     selectCdiColumn.setAttribute('hidden', true);
   } else {
+    /* If false, show column 'Porcentagem CDI'*/
     selectCdiColumn.removeAttribute('hidden');
     selectIndicesColumn.style.gridRow = '1';
     selectIndicesColumn.style.gridColumn = '1';
     selectCdiColumn.style.gridRow = '1';
     selectCdiColumn.style.gridColumn = '2';
   }
+}
+
+selectElement.onchange = function() {
+  if (selectElement.value != 12) {
+    toggleCdiColumn(true);
+  } else {
+    toggleCdiColumn(false);
+  }
+}
+formCalculator.onreset = function() {
+  toggleCdiColumn(false);
 }
